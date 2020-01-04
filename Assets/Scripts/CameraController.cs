665de8000be18a1b812579f6c22c639f29ddc470
George Utsin <georgeutsin@Georges-MacBook-Pro.local>
2020-01-04 13:21:54 -0500

    cameras

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,13 @@ public class CameraController : MonoBehaviour
 {
     public GameObject playerTracker;
 
-    Vector3 gameViewPosition = new Vector3(-22.5f, 45.11f, -8.2f);
+    //Vector3 gameViewPosition = new Vector3(-22.5f, 45.11f, -8.2f);
+    Vector3 gameViewPosition;
 
+    private void Start()
+    {
+        gameViewPosition = transform.position;
+    }
     void LateUpdate()
     {
         transform.position = gameViewPosition + playerTracker.transform.position;
