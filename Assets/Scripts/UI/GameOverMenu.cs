8bdf2846e1cc115194a1e38cfdc5e0a1fd88add7
George Utsin <georgeutsin@gmail.com>
2020-02-15 23:20:11 -0500

    add tagbull integration (#3)

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -1,14 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class GameOverMenu : MonoBehaviour
 {
     public TextMeshProUGUI DetailsText;
+    public TextMeshProUGUI BonusCoinsText;
     public GameObject PlayAgainButton;
     public GameObject MainMenuButton;
+    public GameObject DoubleCoinsButton;
+    public GameObject webview;
+    public GameObject loadingScreen;
+    public Slider bgSlider;
+
+    int bonusCoins;
+    float sliderBGLeft;
+
+    void Update()
+    {
+        UpdateDoubleCoinsButtonBackground();
+    }
+
+    void UpdateDoubleCoinsButtonBackground()
+    {
+        if (sliderBGLeft > 0f)
+        {
+            sliderBGLeft -= Time.deltaTime / 10f;
+        }
+        bgSlider.value = sliderBGLeft;
+
+        if (sliderBGLeft <= 0f)
+        {
+            DoubleCoinsButton.GetComponent<Button>().interactable = false;
+        }
+    }
 
     public void UpdateText(int score, int highscore, int coins, int totalCoins)
     {
@@ -17,18 +42,51 @@ public class GameOverMenu : MonoBehaviour
             "\n" +
             "coins: " + coins + "\n" +
             "total coins: " + totalCoins;
-
+        bonusCoins = coins;
+        BonusCoinsText.text = "";
     }
 
-    public void ShowButtons()
+    public void ShowButtons(bool shouldShowDoubleButton)
     {
         PlayAgainButton.SetActive(true);
         MainMenuButton.SetActive(true);
+        if (shouldShowDoubleButton)
+        {
+            DoubleCoinsButton.GetComponent<Button>().interactable = true;
+            DoubleCoinsButton.SetActive(true);
+            sliderBGLeft = 1f;
+        }
     }
 
     public void HideButtons()
     {
         PlayAgainButton.SetActive(false);
         MainMenuButton.SetActive(false);
+        DoubleCoinsButton.SetActive(false);   
+    }
+
+    public void DoubleCoinsPressed()
+    {
+        Time.timeScale = 0f;
+        StartCoroutine(webview.GetComponent<TagBullWebView>().Create());
+    }
+
+    public void TagBullSuccess()
+    {
+        webview.SetActive(false);
+        loadingScreen.SetActive(false);
+        DoubleCoinsButton.SetActive(false);
+        Time.timeScale = 1.0f;
+
+        StateManager.AddCoins(bonusCoins);
+        BonusCoinsText.text = "bonus coins: " + bonusCoins + "\n" +
+            "new total coins: " + StateManager.GetCoins();
+    }
+
+    public void TagBullCancel()
+    {
+        webview.SetActive(false);
+        loadingScreen.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
---
+++ Assets/Scripts/Utils/TagBullWebView.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TagBullWebView : MonoBehaviour
+{
+    public string Url;
+    GameObject webViewContainer;
+    WebViewObject webViewObject;
+    public GameOverMenu root;
+
+    public IEnumerator Create()
+    {
+        webViewContainer = new GameObject("WebViewObject");
+        webViewObject = webViewContainer.AddComponent<WebViewObject>();
+
+        webViewObject.Init(
+            cb: (msg) =>
+            {
+                Debug.Log(string.Format("CallFromJS[{0}]", msg));
+                if (msg == "success")
+                {
+                    Destroy(webViewContainer);
+                    root.TagBullSuccess();
+                }
+                else if (msg == "cancel")
+                {
+                    Destroy(webViewContainer);
+                    root.TagBullCancel();
+                }
+            },
+            err: (msg) =>
+            {
+                Debug.Log(string.Format("CallOnError[{0}]", msg));
+                //status.text = msg;
+                //status.GetComponent<Animation>().Play();
+            },
+            started: (msg) =>
+            {
+                Debug.Log(string.Format("CallOnStarted[{0}]", msg));
+            },
+            ld: (msg) =>
+            {
+                Debug.Log(string.Format("CallOnLoaded[{0}]", msg));
+#if UNITY_EDITOR_OSX || !UNITY_ANDROID
+                // NOTE: depending on the situation, you might prefer
+                // the 'iframe' approach.
+                // cf. https://github.com/gree/unity-webview/issues/189
+#if true
+                webViewObject.EvaluateJS(@"
+                  if (window && window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.unityControl) {
+                    window.Unity = {
+                      call: function(msg) {
+                        window.webkit.messageHandlers.unityControl.postMessage(msg);
+                      }
+                    }
+                  } else {
+                    window.Unity = {
+                      call: function(msg) {
+                        window.location = 'unity:' + msg;
+                      }
+                    }
+                  }
+                ");
+#else
+                webViewObject.EvaluateJS(@"
+                  if (window && window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.unityControl) {
+                    window.Unity = {
+                      call: function(msg) {
+                        window.webkit.messageHandlers.unityControl.postMessage(msg);
+                      }
+                    }
+                  } else {
+                    window.Unity = {
+                      call: function(msg) {
+                        var iframe = document.createElement('IFRAME');
+                        iframe.setAttribute('src', 'unity:' + msg);
+                        document.documentElement.appendChild(iframe);
+                        iframe.parentNode.removeChild(iframe);
+                        iframe = null;
+                      }
+                    }
+                  }
+                ");
+#endif
+#endif
+                webViewObject.EvaluateJS(@"
+                    Unity.call('ua=' + navigator.userAgent);
+                    window.addEventListener('tagbull', function(e) { Unity.call(e.detail); });
+                ");
+                webViewObject.SetVisibility(true);
+            },
+            //ua: "custom user agent string",
+            enableWKWebView: true);
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        webViewObject.bitmapRefreshCycle = 1;
+#endif
+        webViewObject.SetMargins(5, 5, 5, 5);
+
+#if !UNITY_WEBPLAYER
+        if (Url.StartsWith("http"))
+        {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        }
+        else
+        {
+            var exts = new string[]{
+                ".jpg",
+                ".js",
+                ".html"  // should be last
+            };
+            foreach (var ext in exts)
+            {
+                var url = Url.Replace(".html", ext);
+                var src = System.IO.Path.Combine(Application.streamingAssetsPath, url);
+                var dst = System.IO.Path.Combine(Application.persistentDataPath, url);
+                byte[] result = null;
+                if (src.Contains("://"))
+                {  // for Android
+                    var www = new WWW(src);
+                    yield return www;
+                    result = www.bytes;
+                }
+                else
+                {
+                    result = System.IO.File.ReadAllBytes(src);
+                }
+                System.IO.File.WriteAllBytes(dst, result);
+                if (ext == ".html")
+                {
+                    webViewObject.LoadURL("file://" + dst.Replace(" ", "%20"));
+                    break;
+                }
+            }
+        }
+#else
+        if (Url.StartsWith("http")) {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        } else {
+            webViewObject.LoadURL("StreamingAssets/" + Url.Replace(" ", "%20"));
+        }
+        webViewObject.EvaluateJS(
+            "parent.$(function() {" +
+            "   window.Unity = {" +
+            "       call:function(msg) {" +
+            "           parent.unityWebView.sendMessage('WebViewObject', msg)" +
+            "       }" +
+            "   };" +
+            "});");
+#endif
+        yield break;
+    }
+
+#if !UNITY_WEBPLAYER
+    void OnGUI()
+    {
+
+    }
+#endif
+}
